5bc83647563d94a4647087c48ad80b5cb6b27d59
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-22 12:22:14 -0700

    any7 fn

--- any7.py
+++ any7.py
@@ -1,8 +1,20 @@
 def any7(nums):
-    """Are any of these numbers a 7? (True/False)"""
+    """Are any of these numbers a 7? (True/False)
+        for num in nums:
+        if num == 7:
+            return True
 
-    # YOUR CODE HERE 
+    return False
+
+    """
+
+    # YOUR CODE HERE
+
+    if 7 in nums:
+        return True
+
+    return False
 
 
 print("should be true", any7([1, 2, 7, 4, 5]))
-print("should be false", any7([1, 2, 4, 5]))
+# print("should be false", any7([1, 2, 4, 5]))
