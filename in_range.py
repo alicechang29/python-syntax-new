e2927751da36bfaa1b9e1d892890990ed3e00aba
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-22 12:02:31 -0700

    in_range fn

--- in_range.py
+++ in_range.py
@@ -13,9 +13,13 @@ def in_range(nums, lowest, highest):
 
       20 fits
       30 fits
+
     """
 
-    # YOUR CODE HERE
+    # YOUR CODE HERE - if number is inside nums, if the num is greater than or equal to 15 and less than 30, print
+    for num in nums:
+        if num >= lowest and num <= highest:
+            print(num)
 
 
-in_range([10, 20, 30, 40, 50], 15, 30)            
+in_range([10, 20, 30, 40, 50], 15, 30)
